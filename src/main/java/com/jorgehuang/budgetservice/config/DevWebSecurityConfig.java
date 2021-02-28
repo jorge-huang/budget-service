6f5ff2b4b3abd2c1897abe338bbfb59cafdf36a8
Jorge Huang <jorgehuang27@gmail.com>
2021-02-28 16:59:39 -0500

    Implementing Spring Security

---
+++ src/main/java/com/jorgehuang/budgetservice/config/DevWebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jorgehuang.budgetservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+public class DevWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.headers().frameOptions().sameOrigin();
+    }
+}
