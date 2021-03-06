50a4f007253946602a6ba06599d2f1b2741eaaa1
Jorge Huang <jorgehuang27@gmail.com>
2021-03-06 13:57:35 -0500

    Refactoring WebSecurityConfig to consolidate classes

--- src/main/java/com/jorgehuang/budgetservice/config/WebSecurityConfig.java
+++ src/main/java/com/jorgehuang/budgetservice/config/WebSecurityConfig.java
@@ -1,17 +1,40 @@
 package com.jorgehuang.budgetservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Profile("prod")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
+    private void configureForDevProfile(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.headers().frameOptions().sameOrigin();
+        http.csrf().disable(); // TODO: find out how to implement CSRF
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        if (environment.acceptsProfiles(Profiles.of("dev"))) {
+            configureForDevProfile(http);
+            return;
+        }
+
         http
                 .httpBasic()
                 .and()
