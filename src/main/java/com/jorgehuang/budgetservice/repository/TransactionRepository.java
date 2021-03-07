0e69e559d28575c225e9a92019837069bbef9930
Jorge Huang <jorgehuang27@gmail.com>
2021-03-06 21:10:05 -0500

    Create, update, delete operations for transactions repository

--- src/main/java/com/jorgehuang/budgetservice/repository/TransactionRepository.java
+++ src/main/java/com/jorgehuang/budgetservice/repository/TransactionRepository.java
@@ -20,8 +20,41 @@ public class TransactionRepository {
         return jdbcTemplate.queryForObject("SELECT group_id FROM group_members WHERE username = ?", Integer.class, username);
     }
 
-    public List<Transaction> getAllTransactions() throws DataAccessException {
-        String sql = "SELECT transactions.id, description, category, amount, date, account_id, accounts.name AS account_name FROM transactions JOIN accounts ON account_id = accounts.id AND transactions.group_id = ?";
-        return jdbcTemplate.query(sql, new TransactionMapper(), getCurrentUserGroupId());
+    public List<Transaction> getTransactionsByDateRange(String start, String end) throws DataAccessException {
+        String sql = "SELECT transactions.id, description, category, amount, date, account_id, accounts.name AS account_name FROM transactions JOIN accounts ON account_id = accounts.id AND transactions.group_id = ? AND date BETWEEN ? AND ?";
+        return jdbcTemplate.query(sql, new TransactionMapper(), getCurrentUserGroupId(), start, end);
+    }
+
+    public List<Transaction> getTransactionById(Integer id) throws DataAccessException {
+        String sql = "SELECT transactions.id, description, category, amount, date, account_id, accounts.name AS account_name FROM transactions JOIN accounts ON account_id = accounts.id AND transactions.group_id = ? AND transactions.id = ?";
+        return jdbcTemplate.query(sql, new TransactionMapper(), getCurrentUserGroupId(), id);
+    }
+
+    public int create(Transaction transaction) {
+        String sql = "INSERT INTO transactions (description, category, amount, date, account_id, group_id) VALUES (?, ?, ?, ?, ?, ?)";
+        return jdbcTemplate.update(sql,
+                transaction.getDescription(),
+                transaction.getCategory(),
+                transaction.getAmount(),
+                transaction.getDate(),
+                transaction.getAccountId(),
+                getCurrentUserGroupId());
+    }
+
+    public int update(Transaction transaction) {
+        String sql = "UPDATE transactions SET description = ?, category = ?, amount = ?, date = ?, account_id = ? WHERE id = ? AND group_id = ?";
+        return jdbcTemplate.update(sql,
+                transaction.getDescription(),
+                transaction.getCategory(),
+                transaction.getAmount(),
+                transaction.getDate(),
+                transaction.getAccountId(),
+                transaction.getId(),
+                getCurrentUserGroupId());
+    }
+
+    public int delete(int id) {
+        String sql = "DELETE FROM transactions WHERE id = ? AND group_id = ?";
+        return jdbcTemplate.update(sql, id, getCurrentUserGroupId());
     }
 }
--- src/test/java/com/jorgehuang/budgetservice/integration/repository/TransactionRepositoryIntegrationTest.java
+++ src/test/java/com/jorgehuang/budgetservice/integration/repository/TransactionRepositoryIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.jorgehuang.budgetservice.integration.repository;
 
+import com.jorgehuang.budgetservice.domain.Transaction;
 import com.jorgehuang.budgetservice.repository.TransactionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import java.sql.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,15 +19,60 @@ public class TransactionRepositoryIntegrationTest {
     TransactionRepository repository;
 
     @Test
-    @WithMockUser("user")
-    public void getAllTransactions() {
-        assertEquals(18, repository.getAllTransactions().size());
-        assertEquals(1, repository.getAllTransactions().get(0).getId());
-        assertEquals("food from best grocery", repository.getAllTransactions().get(0).getDescription());
-        assertEquals("food", repository.getAllTransactions().get(0).getCategory());
-        assertEquals(10.50, repository.getAllTransactions().get(0).getAmount());
-        assertEquals("2021-01-01", repository.getAllTransactions().get(0).getDate().toString());
-        assertEquals(1, repository.getAllTransactions().get(0).getAccountId());
-        assertEquals("My Bank Checking A", repository.getAllTransactions().get(0).getAccountName());
+    @WithMockUser()
+    public void getTransactionsByDateRange() {
+        List<Transaction> transactions = repository.getTransactionsByDateRange("2021-01-01", "2021-01-31");
+        assertEquals(2, transactions.size());
+        assertEquals(1, transactions.get(0).getId());
+        assertEquals("food from best grocery", transactions.get(0).getDescription());
+        assertEquals("food", transactions.get(0).getCategory());
+        assertEquals(10.50, transactions.get(0).getAmount());
+        assertEquals("2021-01-01", transactions.get(0).getDate().toString());
+        assertEquals(1, transactions.get(0).getAccountId());
+        assertEquals("My Bank Checking A", transactions.get(0).getAccountName());
+    }
+
+    @Test
+    @WithMockUser()
+    public void getTransactionsById() {
+        List<Transaction> transaction = repository.getTransactionById(1);
+        assertEquals(1, transaction.size());
+        assertEquals(1, transaction.get(0).getId());
+        assertEquals("food from best grocery", transaction.get(0).getDescription());
+        assertEquals("food", transaction.get(0).getCategory());
+        assertEquals(10.50, transaction.get(0).getAmount());
+        assertEquals("2021-01-01", transaction.get(0).getDate().toString());
+        assertEquals(1, transaction.get(0).getAccountId());
+        assertEquals("My Bank Checking A", transaction.get(0).getAccountName());
+    }
+
+    @Test
+    @WithMockUser()
+    public void createUpdateDeleteTransaction() {
+        Transaction transaction = new Transaction();
+        transaction.setDescription("night out");
+        transaction.setCategory("fun");
+        transaction.setAmount(1.50);
+        Date date = new Date(1615081036661L);
+        transaction.setDate(date);
+        transaction.setAccountId(1);
+        assertEquals(1, repository.create(transaction));
+
+        transaction.setId(28);
+        transaction.setDescription("night in");
+        transaction.setCategory("not fun");
+        transaction.setAmount(1.50);
+        date = new Date(1617673036661L);
+        transaction.setDate(date);
+        transaction.setAccountId(2);
+        assertEquals(1, repository.update(transaction));
+        assertEquals(28, repository.getTransactionById(28).get(0).getId());
+        assertEquals("night in", repository.getTransactionById(28).get(0).getDescription());
+        assertEquals("not fun", repository.getTransactionById(28).get(0).getCategory());
+        assertEquals(1.50, repository.getTransactionById(28).get(0).getAmount());
+        assertEquals("2021-04-05", repository.getTransactionById(28).get(0).getDate().toString());
+        assertEquals(2, repository.getTransactionById(28).get(0).getAccountId());
+
+        assertEquals(1, repository.delete(28));
     }
 }
